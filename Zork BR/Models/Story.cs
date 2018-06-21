31a597b83785b3a28e281a29b0b4d8423e87626b
Lars <lars.tebooy@hotmail.com>
2018-06-21 11:54:09 +0200

    Added database for Story and Map

--- Zork BR/Models/Story.cs
+++ Zork BR/Models/Story.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,8 @@ namespace Zork_BR.Models
 {
     public class Story
     {
+        [Key]
+        public int Id { get; set; }
         public string MyStory { get; set; }
     }
 }
