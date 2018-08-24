88d7d08563141f007675017f1a736ad6d73ec6de
LarsteBooy <36959162+LarsteBooy@users.noreply.github.com>
2018-08-24 10:07:38 +0200

    Updated RandomEnemyDeath calculator
    - The more enemies there are the more chance some of them dies
    - When there are 5 enemies left no one randomly dies and the player has to kill them all.

--- Zork BR/Repositories/EnemyRepository.cs
+++ Zork BR/Repositories/EnemyRepository.cs
@@ -35,11 +35,96 @@ namespace Zork_BR.Repositories
         //TODO Juiste random enemy death calculator maken hier. Bijvoorbeeld dat er meer enemies tegelijk kunnen dood gaan en hoe minder enemies er zijn hoe minder kans dat er één dood gaat.
         public void RandomEnemyDeath()
         {
-            int random = Rng.Next(0, 100);
-            if(random <= 30) //30% chance to kill a random enemy
+
+            if(MyStaticClass.EnemiesRemaining > 80) //Executes when there are more than 80 enemies remaining
             {
-                MyStaticClass.EnemiesRemaining--;
+                int chanceToDie = Rng.Next(0, 100);
+                if(chanceToDie < 65) //65% chance enemies will die
+                {
+                    int howManyDeath = Rng.Next(0, 100);
+                    if(howManyDeath < 50) //50% 1 enemy dies
+                    {
+                        MyStaticClass.EnemiesRemaining--;
+                    }
+                    else if(howManyDeath >= 50 && howManyDeath < 86) //35% chance 2 enemies die
+                    {
+                        MyStaticClass.EnemiesRemaining -= 2;
+                    }
+                    else //15% chance 3 enemies die
+                    {
+                        MyStaticClass.EnemiesRemaining -= 3;
+                    }
+                }
             }
+
+            if(MyStaticClass.EnemiesRemaining > 60 && MyStaticClass.EnemiesRemaining <= 80) //Executes when there are more than 60 enemies and less than 80 remaining
+            {
+                int chanceToDie = Rng.Next(0, 100);
+                if (chanceToDie < 35) //35% chance enemies will die
+                {
+                    int howManyDeath = Rng.Next(0, 100);
+                    if (howManyDeath < 65) //65% 1 enemy dies
+                    {
+                        MyStaticClass.EnemiesRemaining--;
+                    }
+                    else if (howManyDeath >= 65 && howManyDeath < 96) //30% chance 2 enemies die
+                    {
+                        MyStaticClass.EnemiesRemaining -= 2;
+                    }
+                    else //5% chance 3 enemies die
+                    {
+                        MyStaticClass.EnemiesRemaining -= 3;
+                    }
+                }
+            }
+
+            if (MyStaticClass.EnemiesRemaining > 40 && MyStaticClass.EnemiesRemaining <= 60) //Executes when there are more than 40 enemies and less than 60 remaining
+            {
+                int chanceToDie = Rng.Next(0, 100);
+                if (chanceToDie < 20) //20% chance enemies will die
+                {
+                    int howManyDeath = Rng.Next(0, 100);
+                    if (howManyDeath < 80) //80% 1 enemy dies
+                    {
+                        MyStaticClass.EnemiesRemaining--;
+                    }
+                    else if (howManyDeath >= 80 && howManyDeath < 100) //19% chance 2 enemies die
+                    {
+                        MyStaticClass.EnemiesRemaining -= 2;
+                    }
+                    else //1% chance 3 enemies die
+                    {
+                        MyStaticClass.EnemiesRemaining -= 3;
+                    }
+                }
+            }
+
+            if (MyStaticClass.EnemiesRemaining > 10 && MyStaticClass.EnemiesRemaining <= 40) //Executes when there are more than 10 enemies and less than 40 remaining
+            {
+                int chanceToDie = Rng.Next(0, 100);
+                if (chanceToDie < 10) //10% chance enemies will die
+                {
+                    int howManyDeath = Rng.Next(0, 100);
+                    if (howManyDeath < 94) //94% 1 enemy dies
+                    {
+                        MyStaticClass.EnemiesRemaining--;
+                    }
+                    else  //6% chance 2 enemies die
+                    {
+                        MyStaticClass.EnemiesRemaining -= 2;
+                    }
+                }
+            }
+
+            if (MyStaticClass.EnemiesRemaining > 4 && MyStaticClass.EnemiesRemaining <= 10) //Executes when there are more than 4 enemies and less than 10 remaining
+            {
+                int chanceToDie = Rng.Next(0, 100);
+                if (chanceToDie < 2) //2% chance an enemy will die
+                {
+                    MyStaticClass.EnemiesRemaining--;
+                }
+            }
+
         }
     }
 }
